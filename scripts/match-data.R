c9f627a3123457073f26825bc327077dfcf800d1
JJ Merelo <jjmerelo@gmail.com>
2017-12-29 09:13:44 +0100

    Fixes script

--- scripts/match-data.R
+++ scripts/match-data.R
@@ -1,13 +1,20 @@
+require(data.table)
+t <- read.csv2("datos/normalizado.csv")
+load("datos/duration.Rdata")
+
 match.data <- data.frame(id=character(),
                          Winner=character(),
                          Loser=character(),
                          Duration=integer())
+
 for( i in 1:nrow(duration)) {
-  id <- duration[i,"id"]
-  t.row <- t[t$id==id,][1,]
-  match.data <- rbind( match.data,
-                       data.frame(id=id,
-                                  Winner=t.row$Winner_Race,
-                                  Loser=t.row$Loser_Race,
-                                  Duration=duration[i,"maxFrames"]))
+    id <- duration[i,"id"]
+    print(paste( "Doing ",i, id ))
+    t.row <- t[t$id==id,][1,]
+    print(paste(t.row$Winner_Race,t.row$Loser_Race))
+    match.data <- rbind( match.data,
+                        data.frame(id=id,
+                                   Winner=t.row$Winner_Race,
+                                   Loser=t.row$Loser_Race,
+                                   Duration=duration[i,"maxFrames"]))
 }
